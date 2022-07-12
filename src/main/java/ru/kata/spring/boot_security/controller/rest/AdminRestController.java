4c0af8d271cae78fd983554e6d8baf998cf6b39d
Alexander735 <happylesa@icloud.com>
2022-07-12 20:49:20 +0300

    WemMvcConfig added instead of controllers admin- and user-

--- src/main/java/ru/kata/spring/boot_security/controller/rest/AdminRestController.java
+++ src/main/java/ru/kata/spring/boot_security/controller/rest/AdminRestController.java
@@ -1,4 +1,4 @@
-package ru.kata.spring.boot_security.rest;
+package ru.kata.spring.boot_security.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
